5970c1bea5cff4c2d2dec106d6b39e22c35add60
Kauã Felix <kfelixcosta11@gmail.com>
2026-04-06 21:53:14 -0300

    feat: injeta repositorys ports

--- src/main/java/kaua/felix/taskflow/domain/service/TaskService.java
+++ src/main/java/kaua/felix/taskflow/domain/service/TaskService.java
@@ -4,12 +4,25 @@ import kaua.felix.taskflow.domain.entity.Task;
 import kaua.felix.taskflow.domain.entity.enuns.TaskStatus;
 import kaua.felix.taskflow.domain.entity.enuns.TypePriority;
 import kaua.felix.taskflow.domain.ports.in.TaskUseCase;
+import kaua.felix.taskflow.domain.ports.out.ProjectRepositoryPort;
+import kaua.felix.taskflow.domain.ports.out.TaskRepositoryPort;
+import kaua.felix.taskflow.domain.ports.out.UserRepositoryPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+@RequiredArgsConstructor
+@Service
 public class TaskService implements TaskUseCase {
+
+    private final TaskRepositoryPort taskRepositoryPort;
+    private final ProjectRepositoryPort projectRepositoryPort;
+    private final UserRepositoryPort userRepositoryPort;
+
+
     @Override
     public Task create(UUID projectId, String title, String description, TypePriority priority, LocalDate deadline, UUID assigneeId, UUID requesterId) {
         return null;
