0243a502db5e99604de1b203bfbb39b5a97dc840
Kaua Felix <kfelixcosta11@gmail.com>
2026-04-07 12:17:19 -0300

    feat: altera o tipo do parâmetro id no método findById de Long para UUID

--- src/main/java/kaua/felix/taskflow/domain/ports/out/UserRepositoryPort.java
+++ src/main/java/kaua/felix/taskflow/domain/ports/out/UserRepositoryPort.java
@@ -3,12 +3,13 @@ package kaua.felix.taskflow.domain.ports.out;
 import kaua.felix.taskflow.domain.entity.User;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepositoryPort {
 
     User save (User user);
 
-    Optional<User> findById(Long id);
+    Optional<User> findById(UUID id);
 
     Optional<User> findByEmail(String email);
 
