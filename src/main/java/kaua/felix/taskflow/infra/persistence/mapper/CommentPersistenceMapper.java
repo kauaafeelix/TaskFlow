ca6c7880fc67e1ca18c3705d22e38ccbc6327461
Kaua Felix <kfelixcosta11@gmail.com>
2026-04-10 00:10:44 -0300

    feat: implementa mapeamento de ProjectMember entre entidades de domínio e JPA

--- src/main/java/kaua/felix/taskflow/infra/persistence/mapper/CommentPersistenceMapper.java
+++ src/main/java/kaua/felix/taskflow/infra/persistence/mapper/CommentPersistenceMapper.java
@@ -1,4 +1,9 @@
 package kaua.felix.taskflow.infra.persistence.mapper;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CommentPersistenceMapper {
+
+
 }
--- src/main/java/kaua/felix/taskflow/infra/persistence/mapper/ProjectMemberPersistenceMapper.java
+++ src/main/java/kaua/felix/taskflow/infra/persistence/mapper/ProjectMemberPersistenceMapper.java
@@ -1,4 +1,34 @@
 package kaua.felix.taskflow.infra.persistence.mapper;
 
+import kaua.felix.taskflow.domain.entity.ProjectMember;
+import kaua.felix.taskflow.infra.persistence.entity.ProjectJpaEntity;
+import kaua.felix.taskflow.infra.persistence.entity.ProjectMemberJpaEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class ProjectMemberPersistenceMapper {
+
+    private final UserPersistenceMapper userMapper;
+
+    public ProjectMember toEntity(ProjectMemberJpaEntity jpa) {
+        return new ProjectMember(
+                jpa.getId(),
+                jpa.getProject().getId(),
+                userMapper.toEntity(jpa.getUser()),
+                jpa.getRole(),
+                jpa.getJoinedAt()
+        );
+    }
+
+    public ProjectMemberJpaEntity toJpa(ProjectMember domain, ProjectJpaEntity projectJpa) {
+        return new ProjectMemberJpaEntity(
+                domain.getId(),
+                projectJpa,
+                userMapper.toJpa(domain.getUser()),
+                domain.getRole(),
+                domain.getJoinedAt()
+        );
+    }
 }
