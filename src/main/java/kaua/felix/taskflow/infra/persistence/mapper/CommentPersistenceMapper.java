f8e72dd0d2f5cec41aca768e7770a2b8eb874c69
Kaua Felix <kfelixcosta11@gmail.com>
2026-04-11 18:50:48 -0300

    feat: implementa mapper da entidade Comment para fazer a conversao da entidade de modelo para entidade JPA

--- src/main/java/kaua/felix/taskflow/infra/persistence/mapper/CommentPersistenceMapper.java
+++ src/main/java/kaua/felix/taskflow/infra/persistence/mapper/CommentPersistenceMapper.java
@@ -1,9 +1,37 @@
 package kaua.felix.taskflow.infra.persistence.mapper;
 
+import kaua.felix.taskflow.domain.entity.Comment;
+import kaua.felix.taskflow.infra.persistence.entity.CommentJpaEntity;
+import kaua.felix.taskflow.infra.persistence.entity.TaskJpaEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class CommentPersistenceMapper {
 
+    private final UserPersistenceMapper userMapper;
+    public Comment toEntity (CommentJpaEntity jpaEntity){
+
+        return new Comment(
+                jpaEntity.getId(),
+                jpaEntity.getTask().getId(),
+                userMapper.toEntity(jpaEntity.getAuthor()),
+                jpaEntity.getContent(),
+                jpaEntity.getCreatedAt(),
+                jpaEntity.getUpdatedAt()
+        );
+    }
+
+    public CommentJpaEntity toJpa(Comment domain, TaskJpaEntity taskJpa) {
+        return new CommentJpaEntity(
+                domain.getId(),
+                taskJpa,
+                userMapper.toJpa(domain.getAuthor()),
+                domain.getContent(),
+                domain.getCreatedAt(),
+                domain.getUpdatedAt()
+        );
+    }
 
 }
