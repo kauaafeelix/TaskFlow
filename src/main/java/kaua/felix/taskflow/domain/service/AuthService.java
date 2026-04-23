3b3d962f608c10576086b68a875c1a8153e78da8
Kauã Felix <kfelixcosta11@gmail.com>
2026-04-23 20:45:20 -0300

    feat: implementa serviço de autenticação com login e registro de usuários

--- src/main/java/kaua/felix/taskflow/domain/service/AuthService.java
+++ src/main/java/kaua/felix/taskflow/domain/service/AuthService.java
@@ -1,16 +1,50 @@
 package kaua.felix.taskflow.domain.service;
 
 import kaua.felix.taskflow.domain.entity.User;
-import kaua.felix.taskflow.domain.ports.in.AuthCase;
+import kaua.felix.taskflow.domain.ports.in.AuthUseCase;
+import kaua.felix.taskflow.domain.ports.out.TokenRepositoryPort;
+import kaua.felix.taskflow.domain.ports.out.UserRepositoryPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-public class AuthService implements AuthCase {
+@Service
+@RequiredArgsConstructor
+public class AuthService implements AuthUseCase {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepositoryPort userRepository;
+    private final TokenRepositoryPort tokenRepositoryPort;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public String login(String email, String password) {
-        return "";
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        email,
+                        password
+                )
+        );
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return tokenRepositoryPort.generateToken(user);
     }
 
     @Override
     public User register(String name, String email, String password) {
-        return null;
+
+        if (userRepository.existsByEmail(email)){
+            throw new RuntimeException("Email already in use");
+        }
+
+        String encodedPassword = passwordEncoder.encode(password);
+
+        User user = User.create(name, email, encodedPassword);
+
+        return userRepository.save(user);
     }
 }
