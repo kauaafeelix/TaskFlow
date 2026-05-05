2a18ef421bc8dd40e18c89cd5ad02cc6fd75dc9b
Kaua Felix <kfelixcosta11@gmail.com>
2026-05-05 11:33:45 -0300

    feat: adiciona DTOs para membros de projeto e atualiza mapeamento de projetos

--- src/main/java/kaua/felix/taskflow/infra/web/dto/project/response/ProjectResponseDto.java
+++ src/main/java/kaua/felix/taskflow/infra/web/dto/project/response/ProjectResponseDto.java
@@ -3,12 +3,14 @@ package kaua.felix.taskflow.infra.web.dto.project.response;
 import kaua.felix.taskflow.domain.entity.enuns.ProjectStatus;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public record ProjectResponseDto(
         UUID id,
         String name,
         ProjectStatus status,
+        List<ProjectMemberResponseDto> members,
         LocalDateTime createdAt
 ) {
 }
--- src/main/java/kaua/felix/taskflow/infra/web/mapper/ProjectWebMapper.java
+++ src/main/java/kaua/felix/taskflow/infra/web/mapper/ProjectWebMapper.java
@@ -1,17 +1,25 @@
 package kaua.felix.taskflow.infra.web.mapper;
 
 import kaua.felix.taskflow.domain.entity.Project;
+import kaua.felix.taskflow.infra.web.dto.project.response.ProjectMemberResponseDto;
 import kaua.felix.taskflow.infra.web.dto.project.response.ProjectResponseDto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProjectWebMapper {
 
-    public ProjectResponseDto toDto(Project project){
+    public ProjectResponseDto toDto(Project project) {
         return new ProjectResponseDto(
                 project.getId(),
                 project.getName(),
                 project.getStatus(),
+                project.getMembers().stream()
+                        .map(member -> new ProjectMemberResponseDto(
+                                member.getUser().getId(),
+                                member.getUser().getName(),
+                                member.getRole()
+                        ))
+                        .toList(),
                 project.getCreatedAt()
         );
     }
